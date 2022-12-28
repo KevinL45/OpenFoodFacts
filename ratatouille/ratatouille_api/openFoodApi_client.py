fda2de0f0dbaa6fdebd93873e7ced19ff4c4bff3
aurelien <aurelien.mouret@imie-paris.fr>
2022-12-28 14:35:53 +0100

    added sample data extracted before and after use extractDataMethod() limit result to 10 data

--- ratatouille/ratatouille_api/openFoodApi_client.py
+++ ratatouille/ratatouille_api/openFoodApi_client.py
@@ -1,5 +1,6 @@
 import requests
 from django.conf import settings
+from ratatouille_api.open_food_data_api_extractor import extractData 
 
 class OpenFoodApi():
     # SEARCH_PRODUCT_URI ='https://fr.openfoodfacts.org/cgi/search.pl?search_terms=Danette&search_simple=1&action=process&json=True'
@@ -10,7 +11,7 @@ class OpenFoodApi():
         if product_name != None and len(product_name) != 0:
             response = requests.get(self.SEARCH_PRODUCT_URI, {'search_terms': product_name})
             if response.status_code == 200:
-                product_data = response.text
+                product_data = extractData(response.text)
             else:
                 product_data = None
         return product_data

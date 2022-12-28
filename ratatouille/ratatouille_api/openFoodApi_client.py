c877813766c9f939c3766993465fdcc355763d19
aurelien <aurelien.mouret@imie-paris.fr>
2022-12-28 15:02:21 +0100

    improved sort filter in URI used for in findProduct to get popular product

--- ratatouille/ratatouille_api/openFoodApi_client.py
+++ ratatouille/ratatouille_api/openFoodApi_client.py
@@ -4,7 +4,7 @@ from ratatouille_api.open_food_data_api_extractor import extractData
 
 class OpenFoodApi():
     # SEARCH_PRODUCT_URI ='https://fr.openfoodfacts.org/cgi/search.pl?search_terms=Danette&search_simple=1&action=process&json=True'
-    SEARCH_PRODUCT_URI ='https://fr.openfoodfacts.org/cgi/search.pl?search_simple=1&action=process&json=true'
+    SEARCH_PRODUCT_URI ='https://fr.openfoodfacts.org/cgi/search.pl?search_simple=1&action=process&sort_by=unique_scans_n&json=true'
 
     def findProduct(self, product_name): 
         product_data = None

afb3b2a3d712cad5c51bb2205db93007ad65bb15
aurelien <aurelien.mouret@imie-paris.fr>
2022-12-29 10:08:13 +0100

    added Product and Menu endpoint by DjangoRestFramework with theirs Serializer

--- ratatouille/ratatouille/urls.py
+++ ratatouille/ratatouille/urls.py
@@ -21,8 +21,9 @@ from ratatouille_api import views
 
 # Routers provide an easy way of automatically determining the URL conf.
 router = routers.DefaultRouter()
+router.register(r'ratatouille_api/menus', views.MenuViewSet)
+router.register(r'ratatouille_api/products', views.ProductViewSet)
 # router.register(r'products', views.ProductViewSet)
-
 
 
 urlpatterns = [
@@ -30,7 +31,7 @@ urlpatterns = [
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     path('ratatouille_api/', include('ratatouille_api.urls')),
     path('admin/', admin.site.urls),
-    path('ratatouille_api/products/', views.products),
+    # path('ratatouille_api/products/', views.products),
 
     path('ratatouille_api/search/product/<str:product_name>', views.search_product)
 
--- ratatouille/ratatouille_api/serializers.py
+++ ratatouille/ratatouille_api/serializers.py
@@ -1,7 +1,12 @@
-from ratatouille_api.models import Product
+from ratatouille_api.models import Product, Menu
 from rest_framework import serializers
 
 class ProductSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Product
-        fields = ['name']
+        fields = ['_id', 'name', 'link', 'users', 'ingredients', 'store', 'categories']
+
+class MenuSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Menu
+        fields = ['_id', 'name', 'dishes']
